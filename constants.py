9e874b4f85f470cc55aabad511f25649f774c442
MariuszAndziak <43908826+MariuszAndziak@users.noreply.github.com>
2023-01-08 15:59:29 +0100

    Sentence loading and preparation

---
+++ constants.py
@@ -0,0 +1,7 @@
+'''This file contains constant values for the project'''
+
+OUTPUT_FOLDER = 'outputs'
+CAPTIONS_FILE_NAME = 'captions.txt'
+FINAL_CAPTIONS_FILE_NAME = 'final_captions.txt'
+
+PLAYLIST = 'https://www.youtube.com/playlist?list=PL22J3VaeABQAOhH1CLMNnMl2R-O1abW1T'
