506cb0a6327443413d16a3059b04608715bafaff
MariuszAndziak <43908826+MariuszAndziak@users.noreply.github.com>
2023-01-08 16:54:11 +0100

    UMAP embeddings

--- constants.py
+++ constants.py
@@ -1,7 +1,14 @@
 '''This file contains constant values for the project'''
 
+# Captions
+PLAYLIST = 'https://www.youtube.com/playlist?list=PL22J3VaeABQAOhH1CLMNnMl2R-O1abW1T'
+
 OUTPUT_FOLDER = 'outputs'
 CAPTIONS_FILE_NAME = 'captions.txt'
 FINAL_CAPTIONS_FILE_NAME = 'final_captions.txt'
 
-PLAYLIST = 'https://www.youtube.com/playlist?list=PL22J3VaeABQAOhH1CLMNnMl2R-O1abW1T'
+# Embeddings
+SENT_LENGTH = 512
+MODELS_FOLDER = 'models'
+SAMPLE_UMAP_MODEL_NAME = 'umap_embeddings'
+
